e08f809d29b12ae73cec9d5f27669b6a9b3544c7
Diogo Sousa <diogogsousa@gmail.com>
2021-12-16 18:24:43 +0000

    Solved advent of code 2021 16_{1,2}.

---
+++ advent_of_code_2021/16_1/src/main.rs
@@ -0,0 +1,129 @@
+use std::io;
+use std::io::{BufRead, BufReader};
+
+#[derive(Debug)]
+enum Expr {
+    Num(u32),
+    Op(Vec<Packet>)
+}
+
+#[derive(Debug)]
+struct Packet {
+    version: u32,
+    expr: Expr,
+}
+
+fn parse_hex_to_bin(data: &str) -> Vec<u8> {
+    let mut bin: Vec<u8> = Vec::with_capacity(4 * data.len());
+
+    for c in data.chars() {
+        let d = c.to_digit(16).unwrap();
+
+        bin.push(((d & 0b1000) >> 3) as u8);
+        bin.push(((d & 0b0100) >> 2) as u8);
+        bin.push(((d & 0b0010) >> 1) as u8);
+        bin.push(((d & 0b0001) >> 0) as u8);
+    }
+
+    bin
+}
+
+fn get_bin_val(bin: &[u8], index: usize, length: usize) -> u32 {
+    let mut r = 0;
+
+    for i in 0..length {
+        r = (r << 1) | (bin[index + i] as u32);
+    }
+
+    r
+}
+
+fn parse_packet(bin: &[u8]) -> (usize, Packet) {
+    let version = get_bin_val(&bin, 0, 3);
+    let type_id = get_bin_val(&bin, 3, 3);
+
+    match type_id {
+        4 => {
+            let mut num = 0;
+            let mut base = 6;
+
+            loop {
+                let v = get_bin_val(&bin, base, 5);
+                let is_last = (v & 0b1_0000) == 0;
+                let v = v & 0b1111;
+
+                num = (num << 4) | v;
+
+                base += 5;
+
+                if is_last {
+                    break;
+                }
+            }
+
+            (base, Packet { version, expr: Expr::Num(num) })
+        },
+        _ => {
+            let length_type = get_bin_val(&bin, 6, 1);
+
+            match length_type {
+                0 => {
+                    let total_subpacket_length = get_bin_val(&bin, 7, 15) as usize;
+                    let start = 7 + 15;
+                    let mut consumed: usize = 0;
+                    let mut operands: Vec<Packet> = Vec::with_capacity(4);
+
+                    while consumed < total_subpacket_length {
+                        let (packet_consumed, packet) = parse_packet(&bin[start + consumed..]);
+                        operands.push(packet);
+                        consumed += packet_consumed;
+                    }
+
+                    (start + consumed, Packet { version, expr: Expr::Op(operands) })
+                },
+                1 => {
+                    let packet_count = get_bin_val(&bin, 7, 11);
+                    let start = 7 + 11;
+                    let mut consumed: usize = 0;
+                    let mut operands: Vec<Packet> = Vec::with_capacity(4);
+
+                    for _ in 0..packet_count {
+                        let (packet_consumed, packet) = parse_packet(&bin[start + consumed..]);
+                        operands.push(packet);
+                        consumed += packet_consumed;
+                    }
+
+                    (start + consumed, Packet { version, expr: Expr::Op(operands) })
+                },
+                _ => unreachable!(),
+            }
+        },
+    }
+}
+
+fn parse(data: &str) -> Packet {
+    let bin: Vec<u8> = parse_hex_to_bin(data);
+
+    parse_packet(&bin).1
+}
+
+fn add_version_nums(packet: &Packet) -> u32 {
+    packet.version + match packet.expr {
+        Expr::Num(_) => 0,
+        Expr::Op(ref packets) => packets.iter().map(add_version_nums).sum(),
+    }
+}
+
+fn main() -> io::Result<()> {
+    let reader = BufReader::new(std::io::stdin());
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let packet = parse(&line);
+
+        // println!("{:#?}", packet);
+        println!("{}", add_version_nums(&packet));
+    }
+
+    Ok(())
+}
