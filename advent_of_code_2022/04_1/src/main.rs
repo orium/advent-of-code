1eb4fc7ddbfd62cccff6310f1ddcd7c43aac8c26
Diogo Sousa <diogogsousa@gmail.com>
2022-12-05 23:27:09 +0000

    Solved advent of code 2022 4_{1,2}.

---
+++ advent_of_code_2022/04_1/src/main.rs
@@ -0,0 +1,16 @@
+const INPUT: &str = include_str!("../input");
+
+fn main() {
+    let count =
+        INPUT
+            .lines()
+            .map(|line| line.split_once(',').unwrap())
+            .map(|(rl, rr)| (rl.split_once('-').unwrap(), rr.split_once('-').unwrap()))
+            .map(|((a, b), (c, d))| (a.parse::<i64>().unwrap(), b.parse::<i64>().unwrap(), c.parse::<i64>().unwrap(), d.parse::<i64>().unwrap()))
+            .map(|(a, b, c, d)| (a..=b, c..=d))
+            .filter(|(rl, rr)|
+                rl.contains(rr.start()) && rl.contains(rr.end()) || rr.contains(rl.start()) && rr.contains(rl.end()))
+            .count();
+
+    println!("{}", count);
+}
